ee3f5c7828680c921a389c3096deff4a802b5c05
Young Hahn <young@mapbox.com>
2014-12-17 14:33:18 -0500

    Add tag publishing.

--- test/publish.sh
+++ test/publish.sh
@@ -6,3 +6,7 @@ main_file="$(dirname $0)/../bin/bundle_node_app"
 apps_dest="s3://mapbox/apps/bundle-node-app"
 aws s3 cp --acl public-read "$main_file" $apps_dest/$gitsha/run
 
+if git describe --tags --exact-match 2> /dev/null; then
+    tag=$(git describe --tags --exact-match)
+    aws s3 cp --acl public-read "$main_file" $apps_dest/$tag/run
+fi
