b5a052d5313c8ccbd8939f85f294869246a8cccb
Young Hahn <young@mapbox.com>
2014-12-17 14:07:54 -0500

    Don't submodule testapp -- tar it up.

--- test/test.sh
+++ test/test.sh
@@ -2,6 +2,7 @@
 
 code="0"
 cwd=$(pwd)
+app=/tmp/bundle-node-app-testapp
 tmp=/tmp/bundle-node-app-test
 
 function assertNo() {
@@ -61,15 +62,17 @@ cd $(dirname $0)
 test=$(pwd)
 bundle_node_app=$(dirname $test)/bin/bundle_node_app
 
-cd $test/testapp
+# setup
+# testapp.tar.gz is a tarball of a git repo (to avoid submodule)
+rm -rf $app
+mkdir -p $app
+tar zxf $test/testapp.tar.gz --directory=$app
+cd $app/testapp
 
 # no args, builds current platform @ master
 assertExit0 "$bundle_node_app"
-assertOk $test/testapp/testapp-linux-x64-master.zip
-echo "PWD $(pwd)"
-ls $test/testapp
-ls $(pwd)
-unpack_bundle $test/testapp/testapp-linux-x64-master.zip
+assertOk $app/testapp/testapp-linux-x64-master.zip
+unpack_bundle $app/testapp/testapp-linux-x64-master.zip
 assertOk $tmp/testapp-linux-x64-master
 assertOk $tmp/testapp-linux-x64-master/v2.txt
 assertOk $tmp/testapp-linux-x64-master/package.json
@@ -79,12 +82,12 @@ assertOk $tmp/testapp-linux-x64-master/node_modules
 assertOk $tmp/testapp-linux-x64-master/node_modules/sqlite3
 assertOk $tmp/testapp-linux-x64-master/node_modules/sqlite3/lib/binding/node-v11-linux-x64
 assertOk $tmp/testapp-linux-x64-master/node_modules/underscore
-rm -f $test/testapp/testapp-linux-x64-master.zip
+rm -f $app/testapp/testapp-linux-x64-master.zip
 
 # builds current platform @ v1.0.0
 assertExit0 "$bundle_node_app v1.0.0"
-assertOk $test/testapp/testapp-linux-x64-v1.0.0.zip
-unpack_bundle $test/testapp/testapp-linux-x64-v1.0.0.zip
+assertOk $app/testapp/testapp-linux-x64-v1.0.0.zip
+unpack_bundle $app/testapp/testapp-linux-x64-v1.0.0.zip
 assertOk $tmp/testapp-linux-x64-v1.0.0
 assertNo $tmp/testapp-linux-x64-v1.0.0/v2.txt
 assertOk $tmp/testapp-linux-x64-v1.0.0/package.json
@@ -94,12 +97,12 @@ assertOk $tmp/testapp-linux-x64-v1.0.0/node_modules
 assertOk $tmp/testapp-linux-x64-v1.0.0/node_modules/sqlite3
 assertOk $tmp/testapp-linux-x64-v1.0.0/node_modules/sqlite3/lib/binding/node-v11-linux-x64
 assertOk $tmp/testapp-linux-x64-v1.0.0/node_modules/underscore
-rm -f $test/testapp/testapp-linux-x64-v1.0.0.zip
+rm -f $app/testapp/testapp-linux-x64-v1.0.0.zip
 
 ## target darwin-x64
 assertExit0 "$bundle_node_app master darwin-x64"
-assertOk $test/testapp/testapp-darwin-x64-master.zip
-unpack_bundle $test/testapp/testapp-darwin-x64-master.zip
+assertOk $app/testapp/testapp-darwin-x64-master.zip
+unpack_bundle $app/testapp/testapp-darwin-x64-master.zip
 assertOk $tmp/testapp-darwin-x64-master
 assertOk $tmp/testapp-darwin-x64-master/v2.txt
 assertOk $tmp/testapp-darwin-x64-master/package.json
@@ -109,7 +112,7 @@ assertOk $tmp/testapp-darwin-x64-master/node_modules
 assertOk $tmp/testapp-darwin-x64-master/node_modules/sqlite3
 assertOk $tmp/testapp-darwin-x64-master/node_modules/sqlite3/lib/binding/node-v11-darwin-x64
 assertOk $tmp/testapp-darwin-x64-master/node_modules/underscore
-rm -f $test/testapp/testapp-darwin-x64-master.zip
+rm -f $app/testapp/testapp-darwin-x64-master.zip
 
 cd $cwd && exit $code
 
