a6bad78a212069369c32f8a2e468aafd5895f199
Young Hahn <young@mapbox.com>
2014-12-17 13:39:56 -0500

    Test bundling @ a gitsha

--- test/test.sh
+++ test/test.sh
@@ -68,6 +68,7 @@ assertExit0 "$bundle_node_app"
 assertOk $test/testapp/testapp-linux-x64-master.zip
 unpack_bundle $test/testapp/testapp-linux-x64-master.zip
 assertOk $tmp/testapp-linux-x64-master
+assertOk $tmp/testapp-linux-x64-master/v2.txt
 assertOk $tmp/testapp-linux-x64-master/package.json
 assertOk $tmp/testapp-linux-x64-master/index.js
 assertNo $tmp/testapp-linux-x64-master/exclude.txt
@@ -77,11 +78,27 @@ assertOk $tmp/testapp-linux-x64-master/node_modules/sqlite3/lib/binding/node-v11
 assertOk $tmp/testapp-linux-x64-master/node_modules/underscore
 rm -f $test/testapp/testapp-linux-x64-master.zip
 
+# builds current platform @ v1.0.0
+assertExit0 "$bundle_node_app v1.0.0"
+assertOk $test/testapp/testapp-linux-x64-v1.0.0.zip
+unpack_bundle $test/testapp/testapp-linux-x64-v1.0.0.zip
+assertOk $tmp/testapp-linux-x64-v1.0.0
+assertNo $tmp/testapp-linux-x64-v1.0.0/v2.txt
+assertOk $tmp/testapp-linux-x64-v1.0.0/package.json
+assertOk $tmp/testapp-linux-x64-v1.0.0/index.js
+assertNo $tmp/testapp-linux-x64-v1.0.0/exclude.txt
+assertOk $tmp/testapp-linux-x64-v1.0.0/node_modules
+assertOk $tmp/testapp-linux-x64-v1.0.0/node_modules/sqlite3
+assertOk $tmp/testapp-linux-x64-v1.0.0/node_modules/sqlite3/lib/binding/node-v11-linux-x64
+assertOk $tmp/testapp-linux-x64-v1.0.0/node_modules/underscore
+rm -f $test/testapp/testapp-linux-x64-v1.0.0.zip
+
 ## target darwin-x64
 assertExit0 "$bundle_node_app master darwin-x64"
 assertOk $test/testapp/testapp-darwin-x64-master.zip
 unpack_bundle $test/testapp/testapp-darwin-x64-master.zip
 assertOk $tmp/testapp-darwin-x64-master
+assertOk $tmp/testapp-darwin-x64-master/v2.txt
 assertOk $tmp/testapp-darwin-x64-master/package.json
 assertOk $tmp/testapp-darwin-x64-master/index.js
 assertNo $tmp/testapp-darwin-x64-master/exclude.txt
